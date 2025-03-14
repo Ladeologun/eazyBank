0417b609e7ff652c48c5c3e9c3262a023e68761a
Ladeologun <ologunomolade21@gmail.com>
2025-03-14 08:18:47 +0100

    chore: added authentication events and session management

--- src/main/java/com/xproj/eazybank/config/EazyBankSecurityConfiguration.java
+++ src/main/java/com/xproj/eazybank/config/EazyBankSecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.xproj.eazybank.config;
 
+import com.xproj.eazybank.exceptionHandlers.CustomAccessDeniedHandler;
+import com.xproj.eazybank.exceptionHandlers.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +16,7 @@ import org.springframework.security.web.authentication.password.HaveIBeenPwnedRe
 
 @Configuration
 @Profile("!prod")
+@EnableMethodSecurity
 public class EazyBankSecurityConfiguration {
 
     @Bean
@@ -25,7 +29,8 @@ public class EazyBankSecurityConfiguration {
                     .anyRequest().authenticated();
         });
         http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
+        http.httpBasic(hbc-> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(hge->hge.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
     }
 
--- src/main/java/com/xproj/eazybank/controllers/BalanceController.java
+++ src/main/java/com/xproj/eazybank/controllers/BalanceController.java
@@ -1,11 +1,13 @@
 package com.xproj.eazybank.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BalanceController {
 
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("/balance")
     public String getBalanceDetails() {
         return "Here are the balance details from the DB";
